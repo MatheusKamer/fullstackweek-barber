45696ec037f521a3d584e1e06d3228ccf3c87c47
Matheus Kamer <matheus_kamer@hotmail.com>
2024-01-30 19:10:48 -0400

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,21 @@
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
+import { capitalizeFirstLetter } from "../_utils/capitalizeFirstLetter";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="px-5 py-6 space-y-1">
+        <h2 className="text-xl font-bold">Olá, Matheus</h2>
+        <p className="text-sm">{
+            capitalizeFirstLetter(format(new Date(), "EEEE',' dd 'de' MMMM", {
+          locale: ptBR,
+          }))}
+        </p>
+      </div>
     </div>
   );
 }
