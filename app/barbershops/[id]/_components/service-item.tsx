506b07a7f56d583811708cef4fc295e5e634b0cb
Matheus Kamer <matheus_kamer@hotmail.com>
2024-02-01 16:57:55 -0400

    feat: add Calendar component

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -1,10 +1,15 @@
 "use client"
 
 import { Button } from "@/app/_components/ui/button";
+import { Calendar } from "@/app/_components/ui/calendar";
 import { Card, CardContent } from "@/app/_components/ui/card";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/app/_components/ui/sheet";
 import { Service } from "@prisma/client";
+import { ptBR } from "date-fns/locale";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
+import { useMemo, useState } from "react";
+import { generateDayTimeList } from "../_helpers/hours";
 
 interface ServiceItemProps {
   service: Service;
@@ -12,13 +17,29 @@ interface ServiceItemProps {
 }
 
 const ServiceItem = ({ service, isAuthenticated }: ServiceItemProps) => {
+  const [date, setDate] = useState<Date | undefined>(new Date())
+  const [hour, setHour] = useState<string | undefined>()
+
+  const handleDateClick = (date: Date | undefined) => {
+    setDate(date)
+    setHour(undefined)
+  }
+
+  const handleHourClick = (time: string) => {
+    setHour(time)
+  }
+
   const handleBookingClick = () => {
     if (!isAuthenticated) {
-      return signIn("google");
+      // return signIn("google");
     }
 
     // TODO abrir modal de agendamento
   }
+
+  const timeList = useMemo(() => {
+    return date ? generateDayTimeList(date) : []
+  }, [date]);
 
   return (
     <Card className="rounded-md">
@@ -47,9 +68,53 @@ const ServiceItem = ({ service, isAuthenticated }: ServiceItemProps) => {
                   currency: "BRL"
                 }).format(service.price)}
               </p>
-              <Button variant="secondary" onClick={handleBookingClick}>
-                Reservar
-              </Button>
+              <Sheet>
+                <SheetTrigger asChild>
+                  <Button variant="secondary" onClick={handleBookingClick}>
+                    Reservar
+                  </Button>
+                </SheetTrigger>
+
+                <SheetContent className="p-0">
+                  <SheetHeader className="text-left px-5 py-6 border-b border-solid border-secondary">
+                    <SheetTitle>Fazer Reserva</SheetTitle>
+                  </SheetHeader>
+
+                  <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={handleDateClick}
+                    locale={ptBR}
+                    fromDate={new Date()}
+                    className="mt-6"
+                    styles={{
+                      head_cell: { width: "100%", textTransform: "capitalize" },
+                      cell: { width: "100%" },
+                      button: { width: "100%" },
+                      nav_button_previous: { width: "32px", height: "32px" },
+                      nav_button_next: { width: "32px", height: "32px" },
+                      caption: { textTransform: "capitalize" }
+                    }}
+                  />
+
+                  {date && (
+                    <div className="flex overflow-x-auto [&::-webkit-scrollbar]:hidden py-6 px-5 gap-3 border-y border-solid border-secondary">
+                      {timeList.map((time) => (
+                        <Button
+                          onClick={() => handleHourClick(time)}
+                          className="rounded-full"
+                          key={time}
+                          variant={
+                            hour === time ? 'default' : 'secondary'
+                          }
+                        >
+                          {time}
+                        </Button>
+                      ))}
+                    </div>
+                  )}
+                </SheetContent>
+              </Sheet>
             </div>
           </div>
         </div>
